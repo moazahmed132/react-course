2666369a0fa0c230ce43322416360e6af066f431
Moaz Ahmed <moaz.ahmed132@gmail.com>
2020-05-04 23:35:10 +0200

    first commit

---
+++ react-course/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>I am a react app </h1>
+    </div>
+  );
+}
+
+export default App;
